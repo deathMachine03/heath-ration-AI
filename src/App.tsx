2c3523f1297147dd6abaeab48a643b189c80feeb
deathMachine03 <s.abeshov@gmail.com>
2026-02-22 23:50:46 +0500

    features

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+function Home() {
+  return <h1>Home</h1>
+}
+
+function About() {
+  return <h1>About</h1>
+}
+
+function NotFound() {
+  return <h1>404 - Page Not Found</h1>
+}
 
 function App() {
-
   return (
-    <>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      
+      {/* redirect example */}
+      <Route path="/main" element={<Navigate to="/" replace />} />
+      
+      {/* fallback */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   )
 }
 
-export default App
+export default App 
